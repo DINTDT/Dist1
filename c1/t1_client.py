721fcc438776fd4bab208bf701578cb8a488395c
Seth-AA <seth98@gmail.com>
2019-10-01 14:17:22 -0300

    just added some ()

--- c1/t1_client.py
+++ c1/t1_client.py
@@ -3,11 +3,11 @@ import socket
 file = open("target.txt","r")
 serv_ip = file.readline().split()[1]
 serv_port = file.readline().split()[1]
-print "connecting to ip:",serv_ip
-print "with port:",serv_port
+print("connecting to ip:",serv_ip)
+print("with port:",serv_port)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((serv_ip, int(serv_port)))
 s.sendall("hello")
 data = s.recv(1024)
-print "got",repr(data)
+print("got",repr(data))
--- s1/t1_serv.py
+++ s1/t1_serv.py
@@ -6,13 +6,12 @@ own_port = 5000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((own_ip, own_port))
 
-print "open @",s.getsockname()
+print("open @",s.getsockname())
 
 s.listen(1)
 
 while True:
 	conn, addr =  s.accept()
-	print "connection from",addr
-	print "received",conn.recv(1024)
+	print("connection from",addr)
+	print("received",conn.recv(1024))
 	conn.sendall("received message")
-
